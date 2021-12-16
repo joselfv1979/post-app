d1542da64227b9f21943985059ad3197330d1a25
jose <jose@gmail.com>
2021-12-16 15:32:20 +0100

    contact links created

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -11,7 +11,11 @@
         >
       </li>
       <li>
-        <fa :icon="['fab', 'github']" /><span>joselfv.1979@gmail.com</span>
+        <fa :icon="['fab', 'github']" /><span
+          ><a href="https://github.com/joselfv1979/post-app"
+            >github.com/joselfv1979/post-app</a
+          ></span
+        >
       </li>
       <li>
         <fa :icon="['fas', 'envelope']" /><span>joselfv.1979@gmail.com</span>
@@ -21,15 +25,28 @@
 </template>
 <style scoped>
 .about {
-  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+h1 {
+  margin-bottom: 3rem;
+}
+
+
+ul {
+  margin: auto
 }
 
 li {
   margin: 1rem;
   font-size: 1.3rem;
+  text-align: justify;
 }
 
 span {
+  color: #fff;
   margin: 0.5rem;
 }
 
