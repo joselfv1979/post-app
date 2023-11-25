c80b9ca32107c0355f565cd2e5dbf7e7eb8d332b
joselfv <joselfv.1979@gmail.com>
2023-11-25 09:02:57 +0100

    update gitignore files

--- api/dist/routes/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const loginController_1 = require("../controllers/loginController");
-const loginRouter = (0, express_1.Router)();
-loginRouter.post("/", loginController_1.loginController);
-exports.default = loginRouter;
-//# sourceMappingURL=loginRoutes.js.map
--- api/dist/test1.js
+++
@@ -1,5 +0,0 @@
-function sayHello(name) {
-    return 'Hi ' + name;
-}
-console.log(sayHello('Hi Ramona'));
-//# sourceMappingURL=test1.js.map
